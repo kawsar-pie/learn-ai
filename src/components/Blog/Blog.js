68ae8a3aa62f42b078c085b8f17012b2de3b3bff
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-04-11 22:42:44 +0600

    blog added

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Blog.css"
+const Blog = (props) => {
+    const {image, title, article_details, reading_duration} = props.blog;
+    return (
+        <div className='blog'>
+            <div className='blog-content'>
+                <img src={image} alt="" />
+                <h2>{title}</h2>
+                <p>{article_details.slice(0,200)+"..."}</p>
+            </div>
+            <div className='time-button'>
+                <p>Time Required: {reading_duration} mins</p>
+                <button className='btn' onClick={()=>props.addReadinTime(reading_duration)} style={{background:"cadetblue",color:"azure"}}>
+                    <h6>Add to list</h6>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
